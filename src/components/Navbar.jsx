9c99f87f44a79fe630b8bf743808fb184ef62f3b
devBuku <shubhayanbagchi30@gmail.com>
2026-02-10 18:16:18 +0530

    tweaks

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,20 +10,40 @@ function Navbar() {
 
       <div className="nav-right">
         <ul className="nav-links">
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/work">Work</Link></li>
-          <li><Link to="/about">About</Link></li>
-          <li><Link to="/contact">Contact</Link></li>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/work">Work</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
         </ul>
 
         <div className="nav-icons">
-          <a href="https://linkedin.com/in/yourusername" target="_blank" rel="noreferrer">
+          <a
+            href="https://linkedin.com/in/yourusername"
+            target="_blank"
+            rel="noreferrer"
+          >
             <FaLinkedinIn />
           </a>
-          <a href="https://github.com/yourusername" target="_blank" rel="noreferrer">
+          <a
+            href="https://github.com/yourusername"
+            target="_blank"
+            rel="noreferrer"
+          >
             <FaGithub />
           </a>
-          <a href="https://twitter.com/yourusername" target="_blank" rel="noreferrer">
+          <a
+            href="https://twitter.com/yourusername"
+            target="_blank"
+            rel="noreferrer"
+          >
             <FaTwitter />
           </a>
           <a href="/resume.pdf" download>
